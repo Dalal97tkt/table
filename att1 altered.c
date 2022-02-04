47faf165c096272cd09a43713ad6d031577614aa
Dalal AL Halabi <77835458+Dalal97tkt@users.noreply.github.com>
2022-02-04 20:04:36 +0100

    Update att1 altered.c

--- att1 altered.c
+++ att1 altered.c
@@ -318,40 +318,4 @@ int main()
  // gmp_printf("c_p=%Zd \n",c_p);
  // gmp_printf("c_q=%Zd \n",c_q);
  printf("%ld\n",mpz_sizeinbase(mod[0],2));
-#include<stdlib.h>
-#include<string.h>
-int main()
-{
- int runs=2;//change to 10000
- char command[50];
- int x=0;
- for(int i=0;i<runs;i++)
- {
-  strcpy( command, "gcc pr_gn1.c -lgmp -lm -o pr_gn1" );
-  system(command);
-  strcpy(command, "./pr_gn1");
-  system(command);
-  strcpy(command, "gcc att1.c -lgmp -lm -o att1" );
-  system(command);
-  //strcpy(command, "./att1");
-  //system(comman);
-  FILE *fp;
-  int status;
-  char path[1035];
-
-  fp = popen("./att1", "r");
-  if (fp == NULL) {
-    printf("Failed to run command\n" );
-    exit;
-  }
- printf("testing\n");
-  /* Read the output a line at a time - output it. */
-  while (fgets(path, sizeof(path), fp) != NULL) {
-    printf("%s", path);
-  }
-  x=x+atoi(path);
- }
- printf("x=%d\n",x);
-}
-  //On peut maintenant passer à l'algorithme LLL avec c_p et c_q
 }
